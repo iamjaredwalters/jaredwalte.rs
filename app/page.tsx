15c586d682b68c8216817f662b42f90e384ba53e
iamjaredwalters <jared.w.walters@gmail.com>
2024-03-04 07:46:31 -0800

    feat: little UI love

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { TextRevealCard } from "@/components/ui/text-reveal-card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-white text-lg mb-2">Jared 🐻</h2>
-      <h3>I am</h3>
+    <main className="bg-[#1d1c20] flex min-h-screen flex-col justify-between p-24">
+      <h2 className="text-white text-lg mb-2">Jared Walters</h2>
+      <h3>About me:</h3>
       <TextRevealCard
         text="Software Engineer"
         revealText="Insatiably Curious"
@@ -13,8 +13,8 @@ export default function Home() {
         text="Dreamer and Builder"
         revealText="Husband and Father"
       />
-      <TextRevealCard text="Runner" revealText="Reader" />
-      <TextRevealCard text="Cooker of food" revealText="Barbecuer of sorts" />
+      <TextRevealCard text="Runner of races" revealText="Reader of books" />
+      <TextRevealCard text="Cooker of food" revealText="BBQer of sorts" />
       <p>Find me:</p>
       <ul>
         <li>
@@ -27,7 +27,7 @@ export default function Home() {
           <a href="https://www.linkedin.com/in/jaredwalters/">LinkedIn</a>
         </li>
       </ul>
-      <p>Reach me:</p>
+      <p>Call me:</p>
       <ul>
         <li>
           <a href="mailto:me@jaredwalte.rs">Email</a>
--- components/ui/text-reveal-card.tsx
+++ components/ui/text-reveal-card.tsx
@@ -56,13 +56,13 @@ export const TextRevealCard = ({
       onMouseMove={mouseMoveHandler}
       ref={cardRef}
       className={cn(
-        "bg-[#1d1c20] border border-white/[0.08] w-[40rem] rounded-lg p-8 relative overflow-hidden",
+        "bg-[#1d1c20]  w-[22rem] rounded-lg px-4 relative overflow-hidden",
         className
       )}
     >
       {children}
 
-      <div className="h-40  relative flex items-center overflow-hidden">
+      <div className="h-40 relative flex items-center overflow-hidden">
         <motion.div
           style={{
             width: "100%",
@@ -84,7 +84,7 @@ export const TextRevealCard = ({
             style={{
               textShadow: "4px 4px 15px rgba(0,0,0,0.5)",
             }}
-            className="text-base sm:text-[3rem] py-10 font-bold text-white bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-300"
+            className="text-base sm:text-[2rem] py-10 font-bold text-white bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-300"
           >
             {revealText}
           </p>
@@ -100,7 +100,7 @@ export const TextRevealCard = ({
         ></motion.div>
 
         <div className=" overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,white,transparent)]">
-          <p className="text-base sm:text-[3rem] py-10 font-bold bg-clip-text text-transparent bg-[#323238]">
+          <p className="text-base sm:text-[2rem] py-10 font-bold bg-clip-text text-transparent bg-[#323238]">
             {text}
           </p>
           <MemoizedStars />
